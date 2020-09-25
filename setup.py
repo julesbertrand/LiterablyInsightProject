d3517b7e177465773638fbf0425f3feb05847519
julesbertrand <julesbertrand13@gmail.com>
2020-09-25 17:32:56 +0200

    changed project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name="NumberOfWordsCorrectPerMinute",
+    name="literacy-insight-project",
     author="Jules Bertrand",
     author_email="julesbertrand13@gmail.com",
     url="https://github.com/julesbertrand/literably-insight-project.git",
