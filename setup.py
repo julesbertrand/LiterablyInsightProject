88855a94bd12366bcf3b971c330b2f545e510ed6
julesbertrand <julesbertrand13@gmail.com>
2020-10-06 13:10:10 +0200

    added install_requires to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="literacy-insight-project",
     author="Jules Bertrand",
     author_email="julesbertrand13@gmail.com",
     url="https://github.com/julesbertrand/literacy-assessment-insight-project.git",
-    packages=["literacy_score"]
+    packages=["literacy_score"],
+    install_requires = [
+      'numpy',
+      'pandas',
+      'joblib',
+      'sklearn',
+      'xgboost',
+      'num2words',
+      'matplotlib',
+      'seaborn',
+      'logging',  # to be removed ?
+      ],
+      zip_safe=False
 )
