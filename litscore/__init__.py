5d8dae8f00364d402687a9a8edbbfa7e4491fa70
julesbertrand <julesbertrand13@gmail.com>
2020-10-07 00:39:53 +0200

    error in package __init__: model was replaced by train.py

--- litscore/__init__.py
+++ litscore/__init__.py
@@ -2,4 +2,4 @@ from litscore.config import *
 from litscore.utils import *
 from litscore.dataset import *
 from litscore.grade import grade_wcpm, DataGrader
-from litscore.models import *
+from litscore.train import *
