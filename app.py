1b4f8c0b64c542c77cfcfac036f3c75fe18ee894
julesbertrand <julesbertrand13@gmail.com>
2020-10-09 15:22:18 +0200

    updated app with erro message when one field or more is blank

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from litreading.grade import grade_wcpm
 
 def main():
     f = open("./appheader.md", 'r')
-    header = st.markdown(f.read())
+    st.markdown(f.read())
     f.close()
     prompt = st.text_area("Input the original text")
     transcript = st.text_area("Input the transcript from the audio")
@@ -14,15 +14,16 @@ def main():
     if st.button("Grade"):
         if prompt == '' or transcript == '' or duration == '':
             st.error('Please fill all fields before grading')
-        df = pd.DataFrame(
-            {
-                'prompt': [prompt],
-                'asr_transcript': [transcript],
-                'scored_duration': [float(duration)]
-            }
-        )
-        grade = grade_wcpm(df)
-        st.write("Your wcpm is %i" % grade['wcpm_estimation'])
+        else:
+            df = pd.DataFrame(
+                {
+                    'prompt': [prompt],
+                    'asr_transcript': [transcript],
+                    'scored_duration': [float(duration)]
+                }
+            )
+            grade = grade_wcpm(df)
+            st.write("Your wcpm is %i" % grade['wcpm_estimation'])
 
 
 if __name__ == "__main__":
