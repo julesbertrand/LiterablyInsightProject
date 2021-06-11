6660f069a4cdc9061713100f44b9e8a16f714633
julesbertrand <julesbertrand13@gmail.com>
2021-06-11 12:49:04 +0200

    enh: use find_package() in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="litreading",
@@ -7,7 +7,7 @@ setup(
     author_email="julesbertrand13@gmail.com",
     url="https://github.com/julesbertrand/litreading-insight-project.git",
     license="MIT license",
-    packages=["litreading"],
+    packages=find_packages(include=["litreading"]),
     install_requires=[
         "numpy",
         "pandas",
