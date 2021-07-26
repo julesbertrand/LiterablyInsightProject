f453093940aeeefbc1b5b91accecc05ddfce235f
julesbertrand <julesbertrand13@gmail.com>
2021-07-26 11:02:47 +0200

    fix: fix pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ from litreading.utils.files import open_file
 
 def test_utils_open_file():
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    test_path = "/test_data/test_data.csv"
+    test_path = "/samples/test_data.csv"
     df = open_file(dir_path + test_path, sep=",")
     df_test = pd.read_csv(dir_path + test_path)
     assert df.equals(df_test)
