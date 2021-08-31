57c9cab4cf406fede1ce9f354e89e40c028dfffa
julesbertrand <julesbertrand13@gmail.com>
2021-08-31 17:02:17 +0200

    fix: imports in tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,7 +4,7 @@ import pytest
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 
-from litreading.model import Model
+from litreading.trainer import ModelTrainer
 
 
 @pytest.mark.parametrize(
@@ -16,7 +16,7 @@ from litreading.model import Model
     ],
 )
 def test_model_init(scaler, estimator, baseline_mode):
-    Model(scaler=scaler, estimator=estimator, baseline_mode=baseline_mode)
+    ModelTrainer(scaler=scaler, estimator=estimator, baseline_mode=baseline_mode)
 
 
 @pytest.mark.parametrize(
@@ -30,7 +30,7 @@ def test_model_init(scaler, estimator, baseline_mode):
 def test_model_fit(scaler, estimator, baseline_mode):
     test_dataset = pd.read_csv("tests/samples/test_data.csv")
 
-    m = Model(
+    m = ModelTrainer(
         scaler=scaler,
         estimator=estimator,
         baseline_mode=baseline_mode,
