4115b92f1753a2fb9d5f69b0a4b3cb504cc68462
julesbertrand <julesbertrand13@gmail.com>
2021-09-03 14:40:43 +0200

    tests: add tests for estimatora dn sclaer Instanciator class

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,11 @@
 import os
 
 import pandas as pd
+import pytest
 
+from sklearn.linear_model import LinearRegression
+
+from litreading.utils.cli import Instanciator
 from litreading.utils.files import open_file
 
 
@@ -11,3 +15,21 @@ def test_utils_open_file():
     df = open_file(dir_path + test_path, sep=",")
     df_test = pd.read_csv(dir_path + test_path)
     assert df.equals(df_test)
+
+
+def test_instanciator_init():
+    available_objects = dict(default=LinearRegression)
+    Instanciator(available_objects, "test object")
+
+
+@pytest.mark.parametrize("parameters", [None, {}, {"normalize": True}])
+def test_instanciator_instanciate(parameters):
+    available_objects = dict(default=LinearRegression)
+    Instanciator(available_objects, "test object").instanciate("default", parameters)
+
+
+def test_instanciator_instanciate_error():
+    available_objects = dict(default=LinearRegression)
+    i = Instanciator(available_objects, "test object")
+    with pytest.raises(ValueError):
+        i.instanciate("key_not_in_dict")
